bdce16242a8c7accc6310400daafbb6089dde35a
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-04-03 20:56:52 +0700

    Try Cupertino App

---
+++ cupertino_app/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:cupertino_app/feeds_page.dart';
+import 'package:cupertino_app/search_page.dart';
+import 'package:cupertino_app/setting_page.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.systemOrange,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Cupertino App'),
+      ),
+      child: Center(
+        child: Text(
+          'Home Page',
+          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.news),
+            label: 'Feeds',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return const FeedsPage();
+          case 1:
+            return const SearchPage();
+          case 2:
+            return const SettingPage();
+          default:
+            return const Center(
+              child: Text('Page not found!'),
+            );
+        }
+      },
+    );
+  }
+}
+
+class SettingsPage {}
